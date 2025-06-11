e9bd2e9599959a8c68830cd25c43b4255dde3fec
yjkxx <yjkxcvii@gmail.com>
2025-06-11 05:59:02 +0000

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Cloud! - Rev. 8:blue deployment'
+    return 'Hello Cloud! - Rev. 9:blue deployment'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
